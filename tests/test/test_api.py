53832c9e13956dcdf70f88b291cdb268eeb88819
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-23 23:10:01 +0400

    step

--- tests/test/test_api.py
+++ tests/test/test_api.py
@@ -2,6 +2,7 @@
 
 make test T=test_api.py
 """
+import json
 import httpretty
 from . import TestCase
 
@@ -17,14 +18,39 @@ class MockHttprettyRequest:
 class TestApi(TestCase):
     """Telemulator API."""
 
+    method = 'testMethod'
+    code = 200
+    data = 'testMethodData'
+    headers = {}
+    answer = (code, headers, json.dumps(data))
+
     def test_emulate_bot_get(self):
         """Emulate_bot with GET method."""
         from telemulator3.api import emulate_bot
 
-        self.telemul.api.answers['testMethod'] = (200, 'testMethodData')
+        self.telemul.api.answers[self.method] = (self.code, self.data)
         func = emulate_bot(self.telemul.api, httpretty.GET)
-        url = self.telemul.bot.token + '/' + 'testMethod?xxx'
-        assert func(MockHttprettyRequest('zzz'), url, {}) == (200, {}, '"testMethodData"')
+
+        url = self.telemul.bot.token + '/{}?xxx'.format(self.method)
+        assert func(MockHttprettyRequest('zzz'), url, self.headers) == self.answer
+
+        url = self.telemul.bot.token + '/{}'.format(self.method)
+        assert func(MockHttprettyRequest('zzz'), url, self.headers) == self.answer
+
+    def test_debug_print(self):
+        """Check debug print."""
+        from telemulator3 import api
+
+        save = api.DEBUG_PRINT
+        api.DEBUG_PRINT = True
+
+        self.telemul.api.answers[self.method] = (self.code, self.data)
+        func = api.emulate_bot(self.telemul.api, httpretty.GET)
+
+        url = self.telemul.bot.token + '/{}'.format(self.method)
+        assert func(MockHttprettyRequest('zzz'), url, self.headers) == self.answer
+
+        api.DEBUG_PRINT = save
 
     def test_fix_ampersand(self):
         """Function fix_ampersand."""
