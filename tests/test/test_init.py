569e5efdc18efee7a649ef9090161933624dcbc8
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-23 11:41:35 +0400

    step

--- tests/test/test_init.py
+++ tests/test/test_init.py
@@ -2,6 +2,7 @@
 
 make test T=test_init.py
 """
+import os
 from . import TestCase
 
 
@@ -15,3 +16,10 @@ class TestInit(TestCase):
         assert self.telemul.bot.name == 'Test bot'
         assert self.telemul.bot.username == 'bot-username'
         assert self.telemul.bot.token == 'xxx-yyy-zzz'
+
+    def test_clean_proxy(self):
+        """Bot clean_proxy method."""
+        os.environ['http_proxy'] = 'xxx'
+        assert 'http_proxy' in os.environ
+        self.telemul.clean_proxy()
+        assert 'http_proxy' not in os.environ
