73517203c9e4efdc602b097b91b5d6e28861f66b
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-24 09:17:19 +0400

    step

--- tests/test/test_api.py
+++ tests/test/test_api.py
@@ -10,9 +10,11 @@ from . import TestCase
 class MockHttprettyRequest:
     """Mock httpretty request."""
 
-    def __init__(self, querystring):
+    def __init__(self, querystring, parsed_body=None, headers=None):
         """Set querystring property."""
         self.querystring = querystring
+        self.parsed_body = parsed_body or {}
+        self.headers = headers or {}
 
 
 class TestApi(TestCase):
@@ -23,6 +25,21 @@ class TestApi(TestCase):
     data = 'testMethodData'
     headers = {}
     answer = (code, headers, json.dumps(data))
+
+    def test_emulate_bot_post(self):
+        """Emulate_bot with POST method."""
+        from telemulator3.api import emulate_bot
+
+        func = emulate_bot(self.telemul.api, httpretty.POST)
+        url = self.telemul.bot.token + '/{}'.format(self.method)
+        request = MockHttprettyRequest(
+          'zzz',
+          parsed_body={
+            'text': 'test text',
+            'other_key': 'otherkey data'
+          }
+        )
+        assert func(request, url, self.headers)[0] == 400
 
     def test_emulate_bot_get(self):
         """Emulate_bot with GET method."""
