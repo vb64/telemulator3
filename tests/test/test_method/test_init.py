8672e9374cd7ac3bed09250216dbc8dfaeadd10f
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-25 17:05:19 +0400

    step

---
+++ tests/test/test_method/test_init.py
@@ -0,0 +1,19 @@
+"""Telemulator API support functions.
+
+make test T=test_method/test_init.py
+"""
+from . import TestMethod
+
+
+class TestInit(TestMethod):
+    """Test __init__.py module."""
+
+    def test_error(self):
+        """Check error function."""
+        from telemulator3.method import error
+
+        code, data = error(100, 'xxx')
+        assert code == 100
+        assert data['ok'] is False
+        assert data['error_code'] == 100
+        assert data['description'] == 'xxx'
