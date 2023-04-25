ef4dbf12550b675eb59ce17fd57277a43a0ebbf3
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-25 17:24:14 +0400

    step

---
+++ telemulator3/method/deleteMessage.py
@@ -0,0 +1,14 @@
+# pylint: disable=invalid-name
+"""Module for deleteMessage method emulator."""
+from . import with_chat, get_int
+
+
+@with_chat
+def response(_api, params, chat):
+    """Delete message from the chat and return True."""
+    # print "#deleteMessage params", params
+    message_id = get_int(params, 'message_id')
+    if message_id in chat.history.messages:
+        del chat.history.messages[message_id]
+
+    return True
