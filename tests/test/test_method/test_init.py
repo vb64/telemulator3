bfcc4bca6fcf3044d3935775e8e337f535b50906
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-26 20:06:07 +0400

    step

--- tests/test/test_method/test_init.py
+++ tests/test/test_method/test_init.py
@@ -2,6 +2,7 @@
 
 make test T=test_method/test_init.py
 """
+import pytest
 from . import TestMethod
 
 
@@ -17,3 +18,15 @@ class TestInit(TestMethod):
         assert data['ok'] is False
         assert data['error_code'] == 100
         assert data['description'] == 'xxx'
+
+    def test_get_chat(self):
+        """Check get_chat function."""
+        from telemulator3.method import get_chat, EmulatorException
+
+        with pytest.raises(EmulatorException) as err:
+            get_chat(self.api, {})
+        assert "no chat_id" in str(err)
+
+        with pytest.raises(EmulatorException) as err:
+            get_chat(self.api, {'chat_id': 666})
+        assert "Forbidden:" in str(err)
