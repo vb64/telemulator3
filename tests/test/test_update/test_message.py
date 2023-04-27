f2b8e326dbbaa8ba9e24d652571df4df26a62a77
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-27 15:23:45 +0400

    step

---
+++ tests/test/test_update/test_message.py
@@ -0,0 +1,64 @@
+"""Telemulator API Message update class.
+
+make test T=test_update/test_message.py
+"""
+from . import TestUpdate
+
+
+class TestMessage(TestUpdate):
+    """Tests for Telegram messages of various types."""
+
+    def test_message(self):
+        """Test comon message."""
+        from telemulator3.update.message import Text, NewChatMembers
+
+        message = Text(self.private, self.teleuser, "Hello!")
+        data = message.to_dict()
+        assert data['text'] == "Hello!"
+
+        group = self.teleuser.create_group("Test group")
+        message = NewChatMembers(group, self.teleuser, [self.api.get_me()])
+        data = message.to_dict()
+        assert data['new_chat_members'][0]['is_bot'] is True
+
+    def test_command(self):
+        """Test Command message."""
+        from telemulator3.update.message import Command
+
+        message = Command(self.private, self.teleuser, "/start")
+        assert message.entities.type == 'bot_command'
+
+    def test_photo(self):
+        """Test Photo message."""
+        from telemulator3.update.message import Photo
+
+        message = Photo(self.private, self.teleuser, 'my image', 100, 100, 200)
+        assert "image 200 bytes with caption: my image" in str(message)
+
+    def test_voice(self):
+        """Test Voice message."""
+        from telemulator3.update.message import Voice
+
+        message = Voice(self.private, self.teleuser, 10, 200)
+        assert "voice 200 bytes 10 seconds" in str(message)
+
+    def test_audio(self):
+        """Test Audio message."""
+        from telemulator3.update.message import Audio
+
+        message = Audio(self.private, self.teleuser, 100, 2000, 'performer', 'mp3 title')
+        assert "audio 2000 bytes 100 seconds" in str(message)
+
+    def test_document(self):
+        """Test Document message."""
+        from telemulator3.update.message import Document
+
+        message = Document(self.private, self.teleuser, 'my_file.txt', 200)
+        assert "document my_file.txt (200 bytes)" in str(message)
+
+    def test_contact(self):
+        """Test Contact message."""
+        from telemulator3.update.message import Contact
+
+        message = Contact(self.private, self.teleuser, '2128506', 'Xxx', 'Yyy', None)
+        assert "contact 2128506" in str(message)
