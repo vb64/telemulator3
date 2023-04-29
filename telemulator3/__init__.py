cb06a4823c492ceeebc788b47d7a43fa7ef676cf
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-29 13:20:18 +0400

    Remove unused code (clean_proxy)

--- telemulator3/__init__.py
+++ telemulator3/__init__.py
@@ -1,5 +1,4 @@
 """Mocked elements of the Telegram Bot API for unit tests."""
-import os
 
 
 class EmulatorException(Exception):
@@ -10,17 +9,11 @@ class Telemulator:
     """Class for Telegram API tests."""
 
     api = None
-    teleuser = None
-    private = None
-    group = None
-    tele_message = None
-    group_message = None
 
     def set_tested_bot(self, bot, username='tested_bot', name='Test Bot'):
         """Set telebot.TeleBot instance for testing."""
         from .api import Telegram
 
-        self.clean_proxy()
         bot.username = username
         bot.name = name
         self.api = Telegram(bot)
@@ -29,16 +22,6 @@ class Telemulator:
     def bot(self):
         """Shortcut for tested bot."""
         return self.api.bot
-
-    @staticmethod
-    def clean_proxy():
-        """Remove system proxy settings.
-
-        Its need for correct proccesing httpretty + requests
-        """
-        for proxy in ['https_proxy', 'http_proxy']:
-            if proxy in os.environ:
-                del os.environ[proxy]
 
     @staticmethod
     def print_trace(is_on):
